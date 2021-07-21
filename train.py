c73cd47f46eff2efa04c67d88623937283b38ccf
Zacks <ggg1000111@gmail.com>
2021-07-21 15:21:15 +0800

    Update train.py

--- train.py
+++ train.py
@@ -51,8 +51,8 @@ def seed_torch(seed=0):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.deterministic = False
 
 
 def build_data_loader( ):
